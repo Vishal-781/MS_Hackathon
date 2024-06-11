aff9ed73de8b492a5b3ca33f13050861cd2ac548
vishalVivahSahyog <vishalshrivastava914419@gmail.com>
2024-06-11 23:01:52 +0530

    first commit

---
+++ main.py
@@ -0,0 +1,67 @@
+from flask import Flask, request, jsonify
+import joblib
+import numpy as np
+
+app = Flask(__name__)
+
+# Load the trained model
+model = joblib.load('knn_model.joblib')
+features = [
+    "itching", "skin_rash", "nodal_skin_eruptions", "continuous_sneezing", "shivering", "chills", "joint_pain", "stomach_pain", 
+    "acidity", "ulcers_on_tongue", "muscle_wasting", "vomiting", "burning_micturition", "spotting_urination", "fatigue", 
+    "weight_gain", "anxiety", "cold_hands_and_feets", "mood_swings", "weight_loss", "restlessness", "lethargy", "patches_in_throat", 
+    "irregular_sugar_level", "cough", "high_fever", "sunken_eyes", "breathlessness", "sweating", "dehydration", "indigestion", 
+    "headache", "yellowish_skin", "dark_urine", "nausea", "loss_of_appetite", "pain_behind_the_eyes", "back_pain", "constipation", 
+    "abdominal_pain", "diarrhoea", "mild_fever", "yellow_urine", "yellowing_of_eyes", "acute_liver_failure", "fluid_overload", 
+    "swelling_of_stomach", "swelled_lymph_nodes", "malaise", "blurred_and_distorted_vision", "phlegm", "throat_irritation", 
+    "redness_of_eyes", "sinus_pressure", "runny_nose", "congestion", "chest_pain", "weakness_in_limbs", "fast_heart_rate", 
+    "pain_during_bowel_movements", "pain_in_anal_region", "bloody_stool", "irritation_in_anus", "neck_pain", "dizziness", "cramps", 
+    "bruising", "obesity", "swollen_legs", "swollen_blood_vessels", "puffy_face_and_eyes", "enlarged_thyroid", "brittle_nails", 
+    "swollen_extremeties", "excessive_hunger", "extra_marital_contacts", "drying_and_tingling_lips", "slurred_speech", "knee_pain", 
+    "hip_joint_pain", "muscle_weakness", "stiff_neck", "swelling_joints", "movement_stiffness", "spinning_movements", "loss_of_balance", 
+    "unsteadiness", "weakness_of_one_body_side", "loss_of_smell", "bladder_discomfort", "foul_smell_of_urine", "continuous_feel_of_urine", 
+    "passage_of_gases", "internal_itching", "toxic_look_(typhos)", "depression", "irritability", "muscle_pain", "altered_sensorium", 
+    "red_spots_over_body", "belly_pain", "abnormal_menstruation", "dischromic_patches", "watering_from_eyes", "increased_appetite", 
+    "polyuria", "family_history", "mucoid_sputum", "rusty_sputum", "lack_of_concentration", "visual_disturbances", 
+    "receiving_blood_transfusion", "receiving_unsterile_injections", "coma", "stomach_bleeding", "distention_of_abdomen", 
+    "history_of_alcohol_consumption", "fluid_overload", "blood_in_sputum", "prominent_veins_on_calf", "palpitations", "painful_walking", 
+    "pus_filled_pimples", "blackheads", "scurring", "skin_peeling", "silver_like_dusting", "small_dents_in_nails", "inflammatory_nails", 
+    "blister", "red_sore_around_nose", "yellow_crust_ooze"
+]
+
+def get_symptoms(s):
+    s = str(s).lower().replace(",", " ").split()
+    fmap = []
+    print(s)
+    for f in features:
+        if f in s:
+            fmap.append(1)
+        else:
+            fmap.append(0)
+    return np.array(fmap).reshape(1,-1)
+
+@app.route("/")
+def hello():
+    return "Hello I am Diago, How can I help you?"
+
+@app.route("/predict", methods=["POST"])
+def predict():
+    # Get JSON data from request
+    data = request.get_json()
+    symptoms = data.get('symptoms')
+
+    if not symptoms:
+        return jsonify({"error": "No symptoms provided"}), 400
+    
+    # Convert symptoms to numpy array for model prediction
+    symptoms_array = get_symptoms(symptoms)
+    print(symptoms_array)
+
+    # Predict the disease
+    prediction = model.predict(symptoms_array)
+
+    # Return the prediction as JSON
+    return jsonify({"predicted_disease": prediction[0]})
+
+if __name__ == '__main__':
+    app.run(debug=True)
